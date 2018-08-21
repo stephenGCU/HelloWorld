659dfcea084603c9981a2cd63fe0496251d3eaef
stephen-javapro <stephen.javapro@gmail.com>
2018-08-21 12:42:46 -0700

    First commit

---
+++ src/main/java/edu/gcu/bootcamp/java/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package edu.gcu.bootcamp.java.helloworld;
+
+public class HelloWorld {
+
+	public static void main (String[] args) {
+		
+		printHelloWorld();
+	}
+	
+	private static void printHelloWorld() {
+		
+		System.out.println("Hello World!");
+	}
+	
+}
